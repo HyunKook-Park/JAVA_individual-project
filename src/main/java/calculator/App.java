f25bb1cb58bb2d89d3da242c11ef8b3b79ab3cca
Robbie <robbie331@>
2024-07-24 13:45:35 +0900

    Level1 -3

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,12 +5,41 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int result = 0;
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
 
-        System.out.println("num1, num2 = " + num1 + " , " + num2);
+            System.out.println("사칙연산 할 기호를 입력하세요");
+            char operator = sc.next().charAt(0);
+
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+                        System.out.println("두번째 숫자를 다시 입력하세요.");
+                        num2=sc.nextInt();
+                    }
+                    result = num1 / num2;
+                    break;
+                default:
+                    System.out.println("올바른 기호를 입력해주세요.");
+                    continue;
+            }
+            System.out.println(result);
+            break;
+        }
     }
 }
