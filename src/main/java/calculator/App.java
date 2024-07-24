0d60dad221518d3b540b28890fdcbb620ea8dfff
Robbie <robbie331@>
2024-07-24 15:03:26 +0900

    Level1 -7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,14 +1,15 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Queue;
 import java.util.LinkedList;
 
 public class App {
     public static void main(String[] args) {
-        int[] arr = new int[3];  // result 저장 변수
+        // int[] arr = new int[3];  // result 저장 변수
         int lastIndex = 0;  // result의 마지막 index 변수 선언
-        
+        ArrayList<Integer> list = new ArrayList<Integer>();
         Scanner sc = new Scanner(System.in);
         int result = 0;
 
@@ -44,17 +45,23 @@ public class App {
                     continue;
             }
             System.out.println("결과"+result);
+            list.add(result);
 
-            arr[lastIndex] = result;
-            ++ lastIndex;
-            if (lastIndex == arr.length - 1) {
-                System.out.println("저장 공간이 가득 찼으므로, 처음 입력했던 결과 값을 삭제하겠습니다.");
-                for(int i=0; i<arr.length - 1; i++) {
-                    arr[i]+=1;
-                }
-                arr[arr.length-1] = result;
+//            arr[lastIndex] = result;
+//            ++ lastIndex;
+//            if (lastIndex == arr.length - 1) {
+//                System.out.println("저장 공간이 가득 찼으므로, 처음 입력했던 결과 값을 삭제하겠습니다.");
+//<1-6>            for(int i=0; i<arr.length - 1; i++) {
+//                    arr[i]+=1;
+//                }
+//                arr[arr.length-1] = result;
+//            }
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            String remove = sc.next();
+
+            if(remove.equals("remove")) {
+                list.remove(0);
             }
-
             System.out.println("더 계산하시겠습니까? (eixt 입력 시 종료)");
             String op = sc.next();
             if(op.equals("exit")) {
